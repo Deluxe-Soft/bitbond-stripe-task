bdd435d5e703e1dab6e2ade4c807feb80a226ac8
Piotr Walkowski <piotr@deluxe-soft.com>
2017-01-24 14:00:37 +0100

    Removal logic from view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,18 @@ class UsersController < ApplicationController
     @user_balance = Stripe::Balance.retrieve(stripe_account: @user.stripe_user_id.to_s)
     @plans = Stripe::Plan.all
 
+    starting_date = (DateTime.now - 3.year).to_time.to_i
+
+    @listed_values = {
+        charges:          Stripe::Charge.list(created: {gte: starting_date}),
+        refunds:          Stripe::Refund.list(created: {gte: starting_date}),
+        disputes:         Stripe::Dispute.list(created: {gte: starting_date}),
+        transfers:        Stripe::Transfer.list(created: {gte: starting_date}),
+        customers:        Stripe::Customer.list,
+        orders:           Stripe::Order.list(created: {gte: starting_date}),
+        returns:          Stripe::OrderReturn.list(created: {gte: starting_date}),
+        subscriptions:    Stripe::Subscription.list(created: {gte: starting_date})
+    }
 
     calculate_montly_stats
     calculate_averages
